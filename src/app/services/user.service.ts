691bbe53782d8ede8622e25b35a4b0249c54693a
BenjaminDelacombaz <benjamin.delacombaz@cpnv.ch>
2019-02-19 08:42:33 +0100

    Create the login function

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { User } from '../models/user';
+import { auth } from 'firebase';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  constructor(private fireAuth: AngularFireAuth) { }
+
+  async login(user: User) {
+    await this.fireAuth.auth.signInWithEmailAndPassword(user.email, user.password)
+    user.id = this.fireAuth.auth.currentUser.uid
+    return user
+  }
+
+  fetchUser(formValues: any): User {
+    return new User('', formValues.email, formValues.password)
+  }
 }
