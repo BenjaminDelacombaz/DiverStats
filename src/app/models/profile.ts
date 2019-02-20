d4cf293e9a9fcd48fb863b3a9dce0debc58d9705
BenjaminDelacombaz <benjamin.delacombaz@cpnv.ch>
2019-02-20 10:30:12 +0100

    Add profile model

---
+++ src/app/models/profile.ts
@@ -0,0 +1,11 @@
+export class Profile {
+    firstname: string
+    lastname: string
+    username: string
+
+    constructor(firstname: string, lastname: string, username: string) {
+        this.firstname = firstname
+        this.lastname = lastname
+        this.username = username
+    }
+}
