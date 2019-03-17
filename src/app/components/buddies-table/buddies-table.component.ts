13c22dc77226019cb8d7f5271cda00e025f99432
BenjaminDelacombaz <benjamin.delacombaz@cpnv.ch>
2019-03-17 14:19:09 +0100

    Create delete action

--- src/app/components/buddies-table/buddies-table.component.ts
+++ src/app/components/buddies-table/buddies-table.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { BuddyService } from 'src/app/services/buddy.service';
+import { MatDialog } from '@angular/material/dialog';
+import { ConfirmComponent } from '../confirm/confirm.component';
+import { Buddy } from 'src/app/models/buddy';
 
 @Component({
   selector: 'app-buddies-table',
@@ -8,11 +11,25 @@ import { BuddyService } from 'src/app/services/buddy.service';
 })
 export class BuddiesTableComponent implements OnInit {
 
-  displayedColumns: string[] = ['firstname', 'lastname', 'email', 'phone', 'birthdate', 'public']
+  displayedColumns: string[] = ['firstname', 'lastname', 'email', 'phone', 'birthdate', 'public', 'action']
 
-  constructor(private buddyService: BuddyService) { }
+  constructor(
+    private buddyService: BuddyService,
+    private dialog: MatDialog
+    ) { }
 
   ngOnInit() {
+  }
+
+  openConfirmDeleteDialog(buddy: Buddy): void {
+    const dialogRef = this.dialog.open(ConfirmComponent,{width: '50%'});
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        // Delete ressource
+        this.buddyService.delete(buddy)
+      }    
+    });
   }
 
 }
--- src/app/components/confirm/confirm.component.ts
+++ src/app/components/confirm/confirm.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-confirm',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ConfirmComponent implements OnInit {
 
-  constructor() { }
+  constructor(private dialogRef: MatDialogRef<ConfirmComponent>) { }
 
   ngOnInit() {
+  }
+
+  cancel() {
+    this.dialogRef.close()
   }
 
 }
