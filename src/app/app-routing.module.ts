ef52baece74e9280f91f153a219837ed357e1826
BenjaminDelacombaz <benjamin.delacombaz@cpnv.ch>
2019-03-17 15:01:07 +0100

    Create route for edit a buddy

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { DivesComponent } from './views/dives/dives.component';
 import { CreateDiveComponent } from './views/create-dive/create-dive.component';
 import { BuddiesComponent } from './views/buddies/buddies.component';
 import { CreateBuddyComponent } from './views/create-buddy/create-buddy.component';
+import { EditBuddyComponent } from './views/edit-buddy/edit-buddy.component';
 
 const routes: Routes = [
   { path: 'dives', component: DivesComponent },
   { path: 'dives/create', component: CreateDiveComponent }, 
   { path: 'buddies', component: BuddiesComponent },
   { path: 'buddies/create', component: CreateBuddyComponent },
+  { path: 'buddies/edit/:id', component: EditBuddyComponent}
 ];
 
 @NgModule({
