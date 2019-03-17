f93fc417b3860db9bb2b5e8ec8eb3a41e74ebd0d
BenjaminDelacombaz <benjamin.delacombaz@cpnv.ch>
2019-03-17 09:28:25 +0100

    Add route and link in toolbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DivesComponent } from './views/dives/dives.component';
 import { CreateDiveComponent } from './views/create-dive/create-dive.component';
+import { BuddiesComponent } from './views/buddies/buddies.component';
 
 const routes: Routes = [
   { path: 'dives', component: DivesComponent },
-  { path: 'dives/create', component: CreateDiveComponent},
+  { path: 'dives/create', component: CreateDiveComponent }, 
+  { path: 'buddies', component: BuddiesComponent },
 ];
 
 @NgModule({
