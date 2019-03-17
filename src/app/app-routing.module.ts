c22bbf2a0d4dd65f6221619ca90dc5577b084e56
BenjaminDelacombaz <benjamin.delacombaz@cpnv.ch>
2019-03-17 09:59:42 +0100

    Add route for create buddy

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { DivesComponent } from './views/dives/dives.component';
 import { CreateDiveComponent } from './views/create-dive/create-dive.component';
 import { BuddiesComponent } from './views/buddies/buddies.component';
+import { CreateBuddyComponent } from './views/create-buddy/create-buddy.component';
 
 const routes: Routes = [
   { path: 'dives', component: DivesComponent },
   { path: 'dives/create', component: CreateDiveComponent }, 
   { path: 'buddies', component: BuddiesComponent },
+  { path: 'buddies/create', component: CreateBuddyComponent },
 ];
 
 @NgModule({
