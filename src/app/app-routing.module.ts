513edacfb8697dfae430d2fc69b767188212d035
BenjaminDelacombaz <benjamin.delacombaz@cpnv.ch>
2019-03-20 09:29:54 +0100

    Add route and link to the route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,13 +5,15 @@ import { CreateDiveComponent } from './views/create-dive/create-dive.component';
 import { BuddiesComponent } from './views/buddies/buddies.component';
 import { CreateBuddyComponent } from './views/create-buddy/create-buddy.component';
 import { EditBuddyComponent } from './views/edit-buddy/edit-buddy.component';
+import { DiveSitesComponent } from './views/dive-sites/dive-sites.component';
 
 const routes: Routes = [
   { path: 'dives', component: DivesComponent },
   { path: 'dives/create', component: CreateDiveComponent }, 
   { path: 'buddies', component: BuddiesComponent },
   { path: 'buddies/create', component: CreateBuddyComponent },
-  { path: 'buddies/edit/:id', component: EditBuddyComponent}
+  { path: 'buddies/edit/:id', component: EditBuddyComponent},
+  { path: 'dive-sites', component: DiveSitesComponent }
 ];
 
 @NgModule({
