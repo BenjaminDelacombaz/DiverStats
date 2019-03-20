3217ac89ceb59cd7197df5e73d26fbb8fc915497
BenjaminDelacombaz <benjamin.delacombaz@cpnv.ch>
2019-03-20 10:38:30 +0100

    Add partial form

--- src/app/components/dive-site-form/dive-site-form.component.ts
+++ src/app/components/dive-site-form/dive-site-form.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { DiveSiteService } from 'src/app/services/dive-site.service';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { DiveSite } from 'src/app/models/dive-site';
+import { GLOBAL } from 'src/app/app.const';
 
 @Component({
   selector: 'app-dive-site-form',
@@ -7,9 +14,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DiveSiteFormComponent implements OnInit {
 
-  constructor() { }
+  private diveSiteForm: FormGroup
+
+  @Input()
+  private diveSite: Observable<DiveSite> = null
+  @Input()
+  private diveSiteId: string = null
+
+  private waterTypes: Array<Object> = []
+  private difficulties: Array<Object> = []
+
+  constructor(private fb: FormBuilder,
+    private snackBar: MatSnackBar,
+    private diveSiteService: DiveSiteService,
+    private router: Router) { }
 
   ngOnInit() {
+    // Set water types
+    this.waterTypes = GLOBAL.waterTypes
+    // Set difficulties
+    this.difficulties = GLOBAL.difficulties
+    // Init form group
+    this.diveSiteForm = this.fb.group({
+      // Initialize all form controls
+      name: ['', Validators.required],
+      water_type: ['', Validators.required],
+      difficulty: ['', Validators.required],
+      location: ['', Validators.required],
+      description: ['', Validators.required]
+    })
+    if (this.diveSiteId != null) {
+      this.diveSite.subscribe(diveSite => {
+        this.diveSiteForm.setValue(
+          {
+            name: diveSite.name,
+            water_type: diveSite.water_type,
+            difficulty: diveSite.difficulty,
+            location: diveSite.location,
+            description: diveSite.description
+          })
+      })
+    }
   }
 
 }
